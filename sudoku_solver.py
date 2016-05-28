bee3f83e60368b9c397260a51e1f3b745e15438f
harianbarasu <hanbarasu@gmail.com>
2016-05-27 22:13:23 -0400

    Added some really crude ways to display the row and column

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -12,8 +12,32 @@ class Board(object):
 
 	# Class function to print the current Board
 	def print_board(self):
+
+		# A really crude way to add the letter headings. 
+		# Come up with a better way.
+
+		print "%3s" %" ",
+		print "%3s" %"A",
+		print "%3s" %"B",
+		print "%3s" %"C",
+		print "%3s" %" ",
+		print "%3s" %"D",
+		print "%3s" %"E",
+		print "%3s" %"F",
+		print "%3s" %" ",
+		print "%3s" %"G",
+		print "%3s" %"H",
+		print "%3s" %"I"
+
+		# A variable to be used to print the row boxes.
+		# Try to think of a better way.\
+		row_counter = 0
+
 		for i in range(9):
 			for j in range(9):
+				if j == 0:
+					print "%3s" %row_counter,
+					row_counter += 1
 				if(i + 1) % 3 == 0 and i != 8 and j == 8:
 					print "%3s" %self.board[i][j]
 				else:
@@ -23,6 +47,7 @@ class Board(object):
 					print "%3s" %"|",
 
 			if (i + 1) % 3 == 0 and i != 8:
+				print " " * 3,
 				for k in range(2):
 					print "-" * 13, "+",
 				print "-" * 13
